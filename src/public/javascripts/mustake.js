24a3dcd1cddd3aa53114e2571f316460f9ad6431
Jr <jr@3on.fr>
2012-07-27 16:45:50 -0700

    this is drawing shit

--- src/public/javascripts/mustake.js
+++ src/public/javascripts/mustake.js
@@ -5,20 +5,41 @@ function Mustake () {
 	var self = this;
 	var loop = false;
 	var images = {};
+	var xUnit = 20;
+	var yUnit = 20;
+
+	function convX(x) {
+		return (canvas.width / 2) + (x * xUnit)
+	}
+
+	function convY(y) {
+		return (canvas.height / 2) + (-y * yUnit)
+	}
 
 
 	function drawMustache(kind, pos){
 		if(kind == "head") var img = images['mustache-head'];
 		else if(kind == "head-other") var img = images['mustache-head-other'];
 		else var img = images['mustache'];
+
+
+		ctx.drawImage(img, 0, 0, img.width, img.height, convX(pos.x), convY(pos.y), img.width / 2, img.height / 2);
 	}
 
 	function drawSnake(mustaches) {
+		console.log("> Draw a snake")
+		if (mustaches.length < 1) { throw "This snake is way to small" };
 
+		drawMustache('head', mustaches[0]);
+
+		for (var i = 1; i < mustaches.length; i++) {
+			drawMustache('tail', mustaches[i]);
+		};
 	}
 
 
 	this.draw = function(data) {
+		console.log("> update display");
 		drawSnake(data.me);
 	}
 
@@ -27,13 +48,13 @@ function Mustake () {
 		var cb = arguments[0];
 		var nbPics = arguments.length - 1;
 		delete arguments[0];
-		console.log('cb:', cb);
+		//console.log('cb:', cb);
 
 		for(i in arguments) {
 			var filename = arguments[i];
 			var name = filename.slice(0, filename.lastIndexOf('.'));
 			
-			console.log(filename, name)
+			//console.log(filename, name)
 
 			images[name] = new Image();
 			images[name].src = "/images/" + filename;
@@ -41,17 +62,21 @@ function Mustake () {
 				--nbPics;
 				//console.log('name: loaded', this.src, "still " + nbPics+ "left")
 				if (nbPics === 0) {
-					console.log("All pictures have been loaded");
+					console.log("> All pictures have been loaded");
 					cb()
 				}
 			};
 
 		}
 	}
+	
+	function letsGo(cb){
+		return function() {
+			//chausette = new Chaussette();
+			console.log("> Game started");
 
-	function letsGo(){
-		//chausette = new Chaussette();
-		console.log("Game started");
+			if(cb) cb()
+		}
 	}
 
 	function init (cb) {
@@ -59,11 +84,11 @@ function Mustake () {
 		ctx = canvas.getContext('2d');
 		loop = true;
 		
-		preaload(letsGo, 'cloud-75.png', 'cloud-95.png', 'mustache-head.png', 'mustache-head-other.png', 'mustache.png');
+		preaload(letsGo(cb), 'cloud-75.png', 'cloud-95.png', 'mustache-head.png', 'mustache-head-other.png', 'mustache.png');
 	}
 
-	this.start = function () {
-		init();
+	this.start = function (cb) {
+		init(cb);
 
 		return self;
 	};
