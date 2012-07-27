db902f420f24fdf2fa807ff9f7c00fa27c502fe0
Jr <jr@3on.fr>
2012-07-27 15:48:44 -0700

    Time to go canvas

--- src/public/javascripts/chaussette.js
+++ src/public/javascripts/chaussette.js
@@ -1,10 +1,15 @@
 function Chaussette () {
-	// render updated game state
-	io.on('update', function(data) {
-	
+	if(!io) {
+		console.log("socket IO is not working!!!")
+		return
 	}
+	
+	// render updated game state
+	io.on('update', function(data){
+	
+	});
 	
 	// emit this on keypress
 	io.emit('keypress', data);
 
-});
+};
--- src/public/javascripts/mustake.js
+++ src/public/javascripts/mustake.js
@@ -1,15 +1,47 @@
 function Mustake () {
 	var canvas;
 	var ctx;
+	var chaussette;
+	var self = this;
+	var loop = false;
+	var images = {};
 
 
-	function init () {
+	this.draw = function(data) {
+		
+	}
+
+	function preaload() {
+		
+		var cb = arguments[0];
+		delete arguments[0];
+		console.log('cb:', cb);
+
+		for(i in arguments) {
+			var filename = arguments[i];
+			var name = filename.slice(0, filename.lastIndexOf('.'));
+			
+			console.log(filename, name)
+
+			images[name] = new Image();
+			images[name].src = "/images/" + filename;
+			images[name].onload = function() { console.log('name: loaded', this.src) };
+
+		}
+	}
+
+
+	function init (cb) {
 		canvas = document.getElementById("mustake");
 		ctx = canvas.getContext('2d');
-
+		loop = true;
+		//chausette = new Chaussette();
+		preaload(function(){}, 'cloud-75.png', 'cloud-95.png', 'mustache-head.png', 'mustache.png');
 	}
 
 	this.start = function () {
+		init();
 
+		return self;
 	};
 }
